9264084ff4343ac9c4def22830b22a0318bf3360
Kyle Jones <kylej@democ.org>
2024-07-03 17:39:35 -0400

    Bump treesitter version

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,7 +2,7 @@ fn main() {
     let src_dir = std::path::Path::new("src");
 
     let mut c_config = cc::Build::new();
-    c_config.include(&src_dir);
+    c_config.include(src_dir);
     c_config
         .flag_if_supported("-Wno-unused-parameter")
         .flag_if_supported("-Wno-unused-but-set-variable")
@@ -27,7 +27,7 @@ fn main() {
 
     let mut cpp_config = cc::Build::new();
     cpp_config.cpp(true);
-    cpp_config.include(&src_dir);
+    cpp_config.include(src_dir);
     cpp_config
         .define("TREE_SITTER_MARKDOWN_AVOID_CRASH", None)
         .flag_if_supported("-Wno-unused-parameter")
